e607fc63c66d86a222fd6ccd48831eb19217ac50
Marian Horoiu <marian.horoiu@gmail.com>
2020-02-05 09:37:36 +0200

    First commit

---
+++ myFirstWebService/myFirstClient/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace myFirstClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            myFirstClient.ServiceReference1.WebService1SoapClient service = new ServiceReference1.WebService1SoapClient();
+
+            string s = service.HelloWorld();
+            double suma = service.Add(5, 7);
+
+            Console.WriteLine(s);
+            Console.WriteLine("Suma este: {0}, suma");
+            Console.ReadKey();
+        }
+    }
+}
